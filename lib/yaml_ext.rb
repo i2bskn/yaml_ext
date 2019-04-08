534e8741d808425934ac9cb055cd13e18db4dee0
i2bskn <i2bskn@craftake.com>
2019-04-08 12:30:30 +0900

    Add example yaml

--- lib/yaml_ext.rb
+++ lib/yaml_ext.rb
@@ -4,8 +4,13 @@ require "yaml_ext/version"
 
 module YamlExt
   class << self
-    def load_file(path)
-      YAML.load(ERB.new(File.read(path), nil, "-").result(binding))
+    def load(path)
     end
+
+    private
+
+      def load_file(path)
+        YAML.load(ERB.new(File.read(path), nil, "-").result)
+      end
   end
 end
